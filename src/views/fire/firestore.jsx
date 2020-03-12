9484be55b536567538ed3c0916fdee50db4da0d4
HemmingAlex <48130294+HemmingAlex@users.noreply.github.com>
2020-03-12 23:54:45 +0000

    plz dont mess with my database
    the api key is gone you fools!!!

--- src/views/fire/firestore.jsx
+++ src/views/fire/firestore.jsx
@@ -5,14 +5,7 @@ import firebase, { firestore } from 'firebase';
 import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 
 const config = {
-    apiKey: "AIzaSyAmHgtoKJhYjr4L2VRTlYWwIJr-GVyk6s8",
-    authDomain: "marcussloss.com.com",
-    databaseURL: "https://marcussloss.com",
-    projectId: "marcuoss-adf73",
-    storageBucket: "marcuoss-adf73.appspot.com",
-    messagingSenderId: "1083670325472",
-    appId: "1:1083670325472:web:f5099b5855331640be0885",
-    measurementId: "G-6G2TG44B1E"
+ nothing to see here, carry on.
     // ...
   };
   firebase.initializeApp(config);
